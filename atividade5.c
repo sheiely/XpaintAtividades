aa97aa6c79dc80be4757f4272f5068c4cc6a8d09
Leudes <108896134+Leudes@users.noreply.github.com>
2024-04-20 12:11:53 -0300

    Update atividade5.c

--- atividade5.c
+++ atividade5.c
@@ -4,23 +4,28 @@ int largura = 600, altura = 600;
 
 /*
 Atividade 5 - Xadrez
+Voce e um amigo são viciados em CHESS.COM a 5 anos, um dia o site do jogo acabou
+caindo. Como jovens do seculo XXI vocês nao tem xadrez em casa, entao para
+compensar resolveram criar uma versao de xadrez no computador.
+
 Nessa atividade voce tem que gerar uma imagem xadrez
-A imagem deve ter 40x40 celulas
-conforme o exemplo
+A imagem deve ter 8x8 casas conforme o exemplo
 */
 
 int main() {
   x_open(largura, altura, "exemplos/Atividade 5");
   /*
   resuloção: 
-  for (int x = 0; x <= 40; x++) {
-    for (int y = 0; y <= 40;y++) {
+  int qntCasas = 8;
+  for (int x = 0; x <= qntCasas; x++) {
+    for (int y = 0; y <= qntCasas; y++) {
       if ((x % 2 == 0 && y % 2 == 0) || (x % 2 != 0 && y % 2 != 0)) {
         x_color_set(WHITE);
       } else {
         x_color_set(BLACK);
       }
-      x_fill_rect(x*(largura/40), y*(altura/40), (largura/40), (altura/40));
+      x_fill_rect(x * (largura / qntCasas), y * (altura / qntCasas),
+                  (largura / qntCasas), (altura / qntCasas));
     }
   }
   */
