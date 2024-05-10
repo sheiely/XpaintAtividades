8e1a453372ec3e7dadaabf7c32e7cce94824f988
Leudes <108896134+Leudes@users.noreply.github.com>
2024-05-10 20:17:08 -0300

    Update questao9.c

--- Bandeiras/questao9.c
+++ Bandeiras/questao9.c
@@ -4,34 +4,31 @@ int largura = 600, altura = 600;
 
 
 int main(){
-    x_open(largura, altura, "exemplos/Questao 9");
+    xopen(largura, altura, "exemplos/Questao 9");
   
     for(int i=0; i < 400; i++){
-
-        /* Resolução:
         int x = rand()%largura;
         int y = rand()%altura;
+
         if(x<y){
             if(x<altura-y){
-                x_color_set(BLUE);  
+                fill("blue");  
             }else{
-                x_color_set(RED);
+                fill("red");
             }  
         }else{
             if(x<altura-y){
-                x_color_set(YELLOW);  
+                fill("yellow");  
             }else{
-                x_color_set(WHITE);
+                fill("white");
             }
         }
-        x_fill_circle(x, y, 10);
-        */
-
-
+        circle(x, y, 10);
+    
 
     }
-    x_save();
-    x_close();
+    save();
+    xclose();
         
     return 0;
 }
