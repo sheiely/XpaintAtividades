1d04ff79b0a39efa3a4bc5d6430cdaf6db3ce9b8
Leudes <108896134+Leudes@users.noreply.github.com>
2024-05-10 20:00:49 -0300

    Update questao1.c

--- Bandeiras/questao1.c
+++ Bandeiras/questao1.c
@@ -4,27 +4,22 @@ int largura = 600, altura = 600;
 
 
 int main(){
-  x_open(largura, altura, "exemplos/Questao 1");
+  xopen(largura, altura, "exemplos/Questao 1");
   
   for(int i=0; i < 400; i++){
-
-
     int x = rand()%largura;//Cria uma posição de x aleatoria
     int y = rand()%altura;//Cria uma posição de y aleatoria
 
     if(x>largura/2){//Aqui temos um if: caso o x seja maior que a metade da largura, ele será pintado de vermelho
-      x_color_set(RED);
+      fill("red");
     }else{
-      x_color_set(BLUE);//Se não, será pintado de azul
+      fill("blue");//Se não, será pintado de azul
     }
-    x_fill_circle(x, y, 10);
-
-
-
+    circle(x, y, 10);
 
   }
-  x_save();
-  x_close();
+  save();
+  xclose();
     
   return 0;
 }
